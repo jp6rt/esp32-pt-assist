d3c021b9dfb6ed19967a9faafd4ae8b79e58d195
Joey Resuento <j.resuento@arcanys.com>
2023-08-28 15:59:30 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,102 @@
+import machine
+import dht
+from machine import Pin, SoftI2C, ADC, RTC
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
+from time import sleep
+
+I2C_ADDR = 0x27
+totalRows = 2
+totalColumns = 16
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)
+lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
+
+rtc = RTC()
+sensor = dht.DHT11(Pin(14))
+
+podomoro_btn_start = Pin(17, Pin.IN)
+podomoro_btn_reset = Pin(16, Pin.IN)
+podomoro_cntr = 0
+# Podomoro time in seconds
+podomoro = 1500
+podomoro_state = False
+
+daily_clk_btn_start = Pin(18, Pin.IN)
+daily_clk_btn_pause = Pin(19, Pin.IN)
+daily_clk_cntr = 0
+daily_clk_is_counting = False
+
+def to_str_pad(i):
+    return "0{}".format(i) if len(str(i)) == 1 else str(i)
+
+def get_hms_str():
+    """
+    Return the hh:mm:ss in this format from RTC
+    """
+    
+    # remove seconds to make room for daily counter
+    # hms = rtc.datetime()[4:7]
+    hms = rtc.datetime()[4:6]
+    
+    return ':'.join(list(map(to_str_pad, hms)))
+
+def podomoro_disp():
+    t_remaining = podomoro - podomoro_cntr
+    minutes = int(t_remaining / 60)
+    seconds = t_remaining % 60
+    return "{}:{}".format(to_str_pad(minutes), to_str_pad(seconds))
+
+def daily_clk_disp():
+    hours = int(daily_clk_cntr / 3600)
+    minutes = int((daily_clk_cntr % 3600) / 60)
+    seconds = daily_clk_cntr % 60
+    return "{}:{}:{}".format(to_str_pad(hours), to_str_pad(minutes), to_str_pad(seconds))
+
+def update_display(force_update=False):
+    """
+    Updates the lcd display every 5s
+    """
+    # current_seconds = rtc.datetime()[6:7][0]
+    # if not current_seconds % 5 == 0 and not force_update:
+    #    return
+    
+    sensor.measure()
+    sensor_disp = "{}C {}Ha".format(sensor.temperature(), sensor.humidity())
+    
+    # For some reason instead of clearing the lcd which causes an animation when
+    # writing characters to the cursors, writing to the x=0, y=0 cursor appears to
+    # work without issues
+    # lcd.clear()
+    lcd.move_to(0, 0)
+    lcd.putstr("{} {}\n{} {}".format(get_hms_str(), sensor_disp, podomoro_disp(), daily_clk_disp()))
+
+while True:
+    if podomoro_btn_start.value() == 1:
+        podomoro_state = True
+        update_display(True)
+        
+    if podomoro_btn_reset.value() == 1:
+        podomoro_state = False
+        podomoro_cntr = 0
+        update_display(True)
+    
+    if podomoro_cntr >= podomoro:
+        podomoro_state = False
+        podomoro_cntr = 0
+        update_display(True)
+    
+    if daily_clk_btn_start.value() == 1:
+        daily_clk_is_counting = True
+        update_display(True)
+        
+    if daily_clk_btn_pause.value() == 1:
+        daily_clk_is_counting = False
+        update_display(True)
+    
+    if podomoro_state:
+        podomoro_cntr = podomoro_cntr + 1
+        
+    if daily_clk_is_counting:
+        daily_clk_cntr = daily_clk_cntr + 1
+
+    update_display()
